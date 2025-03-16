87eb154303f96d3408afde65d21fcff7a2ddcfb3
Marcel Wegmann <marcel.wegmann@outlook.com>
2025-03-16 18:41:15 +0100

    🍵 The great purge.
    Signed-off-by: Marcel Wegmann <marcel.wegmann@outlook.com>

--- fish/config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    source /home/marcel/.config/fish/kanagawa-paper.fish
-end
-
-if status is-interactive
-and not set -q TMUX
-    exec tmux
-end
--- nvim/lua/plugins/themes/kanagawa-paper.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"sho-87/kanagawa-paper.nvim",
-	lazy = false,
-	priority = 1000,
-	opts = {},
-	config = function()
-		vim.cmd("colorscheme kanagawa-paper")
-	end,
-}
